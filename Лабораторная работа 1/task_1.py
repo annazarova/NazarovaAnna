b0f03c8643568f726f737881fa5acb93d3a1e724
annazarova <nazarova-anna012@mail.ru>
2025-12-08 18:12:31 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task_1.py
@@ -0,0 +1,12 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+none_index = numbers.index(None)
+sum_without_none = sum(x for x in numbers if x is not None)
+
+count_total = len(numbers)
+average = sum_without_none / count_total
+
+numbers[none_index] = average
+# TODO заменить значение пропущенного элемента средним арифметическим
+
+print("Измененный список:", numbers)
